dea874907054255ef580a170de64bd0c9e506c70
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-02-16 14:48:47 -0700

    chore: code cleanup for first test

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,10 @@
 import { Router } from 'itty-router'
+import StacksBlockHeight from './handlers/stacksblockheight'
 
 const router = Router()
 
 router
-  .get('/stacks-block-height')
-  .get('/totalsupply')
-  .get('/blockreward')
+  .get('/stacks-block-height', StacksBlockHeight)
   .get('*', () => new Response("Not found", { status: 404 }))
 
 export const handleRequest = (request: Request): string => router.handle(request)
