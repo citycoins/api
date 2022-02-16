ccb8e24cfc37921e2abaf40d6e3ce6e63e7b95f8
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-02-16 14:34:59 -0700

    feat: create basic router

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,11 @@
-export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
-}
+import { Router } from 'itty-router'
+
+const router = Router()
+
+router
+  .get('/stacks-block-height')
+  .get('/totalsupply')
+  .get('/blockreward')
+  .get('*', () => new Response("Not found", { status: 404 }))
+
+export const handleRequest = (request: Request): string => router.handle(request)
