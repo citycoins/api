8929ee614a3d67a303d342b0720629ccebb69bb3
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-02-22 08:56:31 -0700

    feat: add type and function for single values

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -1,3 +1,8 @@
 import { StacksMainnet } from "micro-stacks/network"
+import { SingleValue } from "../types/common";
 
 export const STACKS_NETWORK = new StacksMainnet();
+
+export async function createSingleValue(value: string): Promise<SingleValue> {
+  return { value: value }
+}
