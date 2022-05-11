e4b5edbbf4dfdeaa2b14a28fb349d29878267de6
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-05-11 08:40:08 -0700

    fix: update citycoins library to use new types
    This updates the main functions to accept a CityVersion instead of CityConfig, and to use the new naming structure for calilng ccontract functions.

--- src/lib/citycoins.ts
+++ src/lib/citycoins.ts
@@ -1,6 +1,6 @@
 import { fetchReadOnlyFunction } from 'micro-stacks/api'
 import { standardPrincipalCV, uintCV } from 'micro-stacks/clarity'
-import { CityConfig } from '../types/cities'
+import { CityVersion } from '../types/cities'
 import { MinerAtBlock, MiningStatsAtBlock } from '../types/mining'
 import { StackerAtCycle, StackingStatsAtCycle } from '../types/stacking'
 import { CoinbaseThresholds } from '../types/token'
@@ -11,47 +11,47 @@ import { getStacksBlockHeight } from './stacks'
 // ACTIVATION FUNCTIONS
 //////////////////////////////////////////////////
 
-export async function getActivationBlock(cityConfig: CityConfig): Promise<string> {
+export async function getActivationBlock(cityConfig: CityVersion): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-activation-block',
     functionArgs: [],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer
+    senderAddress: cityConfig.contracts.deployer
   }, true)
 }
 
-export async function getRegisteredUsersNonce(cityConfig: CityConfig): Promise<string> {
+export async function getRegisteredUsersNonce(cityConfig: CityVersion): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-registered-users-nonce',
     functionArgs: [],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer
+    senderAddress: cityConfig.contracts.deployer
   }, true)
 }
 
-export async function getUser(cityConfig: CityConfig, id: string): Promise<string> {
+export async function getUser(cityConfig: CityVersion, id: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-user',
     functionArgs: [uintCV(id)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getUserId(cityConfig: CityConfig, address: string): Promise<string> {
+export async function getUserId(cityConfig: CityVersion, address: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-user-id',
     functionArgs: [standardPrincipalCV(address)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
@@ -59,58 +59,58 @@ export async function getUserId(cityConfig: CityConfig, address: string): Promis
 // MINING FUNCTIONS
 //////////////////////////////////////////////////
 
-export async function getBlockWinnerId(cityConfig: CityConfig, blockHeight: string): Promise<string> {
+export async function getBlockWinnerId(cityConfig: CityVersion, blockHeight: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-block-winner-id',
     functionArgs: [uintCV(blockHeight)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getMiningStatsAtBlock(cityConfig: CityConfig, blockHeight: string): Promise<MiningStatsAtBlock> {
+export async function getMiningStatsAtBlock(cityConfig: CityVersion, blockHeight: string): Promise<MiningStatsAtBlock> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-mining-stats-at-block',
     functionArgs: [uintCV(blockHeight)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getMinerAtBlock(cityConfig: CityConfig, blockHeight: string, userId: string): Promise<MinerAtBlock> {
+export async function getMinerAtBlock(cityConfig: CityVersion, blockHeight: string, userId: string): Promise<MinerAtBlock> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-miner-at-block',
     functionArgs: [uintCV(blockHeight), uintCV(userId)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getLastHighValueAtBlock(cityConfig: CityConfig, blockHeight: string): Promise<string> {
+export async function getLastHighValueAtBlock(cityConfig: CityVersion, blockHeight: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-last-high-value-at-block',
     functionArgs: [uintCV(blockHeight)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function hasMinedAtBlock(cityConfig: CityConfig, blockHeight: string, userId: string): Promise<string> {
+export async function hasMinedAtBlock(cityConfig: CityVersion, blockHeight: string, userId: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'has-mined-at-block',
     functionArgs: [uintCV(blockHeight), uintCV(userId)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
@@ -118,25 +118,25 @@ export async function hasMinedAtBlock(cityConfig: CityConfig, blockHeight: strin
 // MINING CLAIM FUNCTIONS
 //////////////////////////////////////////////////
 
-export async function canClaimMiningReward(cityConfig: CityConfig, address: string, blockHeight: string): Promise<string> {
+export async function canClaimMiningReward(cityConfig: CityVersion, address: string, blockHeight: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'can-claim-mining-reward',
     functionArgs: [standardPrincipalCV(address), uintCV(blockHeight)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function isBlockWinner(cityConfig: CityConfig, address: string, blockHeight: string): Promise<string> {
+export async function isBlockWinner(cityConfig: CityVersion, address: string, blockHeight: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'is-block-winner',
     functionArgs: [standardPrincipalCV(address), uintCV(blockHeight)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
@@ -144,58 +144,58 @@ export async function isBlockWinner(cityConfig: CityConfig, address: string, blo
 // STACKING FUNCTIONS
 //////////////////////////////////////////////////
 
-export async function getStackingStatsAtCycle(cityConfig: CityConfig, cycleId: string): Promise<StackingStatsAtCycle> {
+export async function getStackingStatsAtCycle(cityConfig: CityVersion, cycleId: string): Promise<StackingStatsAtCycle> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-stacking-stats-at-cycle',
     functionArgs: [uintCV(cycleId)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getStackerAtCycle(cityConfig: CityConfig, cycleId: string, userId: string): Promise<StackerAtCycle> {
+export async function getStackerAtCycle(cityConfig: CityVersion, cycleId: string, userId: string): Promise<StackerAtCycle> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-stacker-at-cycle',
     functionArgs: [uintCV(cycleId), uintCV(userId)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getRewardCycle(cityConfig: CityConfig, blockHeight: string): Promise<string> {
+export async function getRewardCycle(cityConfig: CityVersion, blockHeight: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-reward-cycle',
     functionArgs: [uintCV(blockHeight)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getFirstStacksBlockInRewardCycle(cityConfig: CityConfig, cycleId: string): Promise<string> {
+export async function getFirstStacksBlockInRewardCycle(cityConfig: CityVersion, cycleId: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-first-stacks-block-in-reward-cycle',
     functionArgs: [uintCV(cycleId)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function stackingActiveAtCycle(cityConfig: CityConfig, cycleId: string): Promise<string> {
+export async function stackingActiveAtCycle(cityConfig: CityVersion, cycleId: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'stacking-active-at-cycle',
     functionArgs: [uintCV(cycleId)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
@@ -203,14 +203,14 @@ export async function stackingActiveAtCycle(cityConfig: CityConfig, cycleId: str
 // STACKING CLAIM FUNCTIONS
 //////////////////////////////////////////////////
 
-export async function getStackingReward(cityConfig: CityConfig, cycleId: string, userId: string): Promise<string> {
+export async function getStackingReward(cityConfig: CityVersion, cycleId: string, userId: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-stacking-reward',
     functionArgs: [uintCV(userId), uintCV(cycleId)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
@@ -218,91 +218,91 @@ export async function getStackingReward(cityConfig: CityConfig, cycleId: string,
 // TOKEN FUNCTIONS
 //////////////////////////////////////////////////
 
-export async function getCoinbaseThresholds(cityConfig: CityConfig): Promise<CoinbaseThresholds> {
+export async function getCoinbaseThresholds(cityConfig: CityVersion): Promise<CoinbaseThresholds> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.tokenContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.token,
     functionName: 'get-coinbase-thresholds',
     functionArgs: [],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getCoinbaseAmount(cityConfig: CityConfig, blockHeight: string): Promise<string> {
+export async function getCoinbaseAmount(cityConfig: CityVersion, blockHeight: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.coreContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.core,
     functionName: 'get-coinbase-amount',
     functionArgs: [uintCV(blockHeight)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getName(cityConfig: CityConfig): Promise<string> {
+export async function getName(cityConfig: CityVersion): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.tokenContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.token,
     functionName: 'get-name',
     functionArgs: [],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getSymbol(cityConfig: CityConfig): Promise<string> {
+export async function getSymbol(cityConfig: CityVersion): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.tokenContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.token,
     functionName: 'get-symbol',
     functionArgs: [],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getDecimals(cityConfig: CityConfig): Promise<string> {
+export async function getDecimals(cityConfig: CityVersion): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.tokenContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.token,
     functionName: 'get-decimals',
     functionArgs: [],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getBalance(cityConfig: CityConfig, address: string): Promise<string> {
+export async function getBalance(cityConfig: CityVersion, address: string): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.tokenContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.token,
     functionName: 'get-balance',
     functionArgs: [standardPrincipalCV(address)],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getTotalSupply(cityConfig: CityConfig): Promise<string> {
+export async function getTotalSupply(cityConfig: CityVersion): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.tokenContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.token,
     functionName: 'get-total-supply',
     functionArgs: [],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
-export async function getTokenUri(cityConfig: CityConfig): Promise<string> {
+export async function getTokenUri(cityConfig: CityVersion): Promise<string> {
   return fetchReadOnlyFunction({
-    contractAddress: cityConfig.deployer,
-    contractName: cityConfig.tokenContract,
+    contractAddress: cityConfig.contracts.deployer,
+    contractName: cityConfig.contracts.token,
     functionName: 'get-token-uri',
     functionArgs: [],
     network: STACKS_NETWORK,
-    senderAddress: cityConfig.deployer,
+    senderAddress: cityConfig.contracts.deployer,
   }, true)
 }
 
@@ -310,7 +310,7 @@ export async function getTokenUri(cityConfig: CityConfig): Promise<string> {
 // UTILITY FUNCTIONS
 //////////////////////////////////////////////////
 
-export async function getProofOfHodl(cityConfig: CityConfig, address: string): Promise<boolean> {
+export async function getProofOfHodl(cityConfig: CityVersion, address: string): Promise<boolean> {
   // check if the user has a balance
   // if so, return true
   const balance = await getBalance(cityConfig, address)
