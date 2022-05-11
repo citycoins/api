886079d2fdda1da577180847ba7810c4bf1d9f34
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-05-11 09:31:56 -0700

    fix: refactor data to follow protocol/contract data
    Third time's a charm! By separating out the variables by contract type (auth/core/token) it allows us to grab the relevant value to a contract (is auth activated?) as well as infer where to look for the data if there is a discrepancy.
    
    This could also allow defining objects for the v1/v2/vN contracts that could be reused say, if the same auth/token contracts are used in a new version alongside a different core contract.

--- src/types/cities.ts
+++ src/types/cities.ts
@@ -1,35 +1,52 @@
 // CONFIGURATION
 
+// cityName: New York City
+// status: ContractStatus
+  // deployed: true
+  // deployer: SPSCWDV3RKV5ZRN1FQD84YE1NQFEDJ9R1F4DYQ11
+// auth: AuthContract
+  // name: newyorkcitycoin-auth-v2
+  // initialized: boolean
+// core: CoreContract
+  // name: newyorkcitycoin-core-v2
+  // activated
+  // shutdown
+// token: TokenContract
+  // name: newyorkcitycoin-token-v2
+  // activated: boolean
+
+export interface CityVersions {
+  [version: string]: CityConfig
+}
+
 export interface CityConfig {
-  [version: string]: CityVersion
-}
-
-export interface CityVersion {
   cityName: string,
-  contracts: CityContracts,
-  status: CityStatus,
-  token: CityToken,
+  deployed: boolean,
+  deployer?: string,
+  auth: AuthContract,
+  core: CoreContract,
+  token: TokenContract,
 }
 
-export interface CityContracts {
-  auth: string,
-  core: string,
-  token: string,
-  deployer: string,
+export interface AuthContract {
+  name: string,
+  initialized: boolean,
 }
 
-export interface CityStatus {
+export interface CoreContract {
+  name: string,
   activated: boolean,
   startBlock?: number,
-  deployed: boolean,
   shutdown: boolean,
   shutdownBlock?: number,
 }
 
-export interface CityToken {
+export interface TokenContract {
+  name: string,
+  activated: true,
   activationBlock?: number,
   displayName: string,
-  name: string,
+  tokenName: string,
   symbol: string,
   decimals: number,
   logo: string,
@@ -38,101 +55,120 @@ export interface CityToken {
 
 // MIAMICOIN
 
-const miaConfig: CityConfig = {
+const miaConfig: CityVersions = {
   'v1': {
     cityName: 'Miami',
-    contracts: {
-      auth: 'miamicoin-auth',
-      core: 'miamicoin-core-v1',
-      token: 'miamicoin-token',
-      deployer: 'SP466FNC0P7JWTNM2R9T199QRZN1MYEDTAR0KP27',
+    deployed: true,
+    deployer: 'SP466FNC0P7JWTNM2R9T199QRZN1MYEDTAR0KP27',
+    auth: {
+      name: 'miamicoin-auth',
+      initialized: true,
     },
-    status: {
+    core: {
+      name: 'miamicoin-core-v1',
       activated: false,
-      deployed: false,
-      shutdown: false,
+      startBlock: 24497,
+      shutdown: true,
+      shutdownBlock: 58917,
     },
     token: {
+      name: 'miamicoin-token',
+      activated: true,
+      activationBlock: 24497,
       displayName: 'MiamiCoin',
-      name: 'miamicoin',
+      tokenName: 'miamicoin',
       symbol: 'MIA',
       decimals: 0,
       logo: 'https://cdn.citycoins.co/logos/miamicoin.png',
       uri: 'https://cdn.citycoins.co/metadata/miamicoin.json',
-    }
+    },
   },
   'v2': {
     cityName: 'Miami',
-    contracts: {
-      auth: 'miamicoin-auth-v2',
-      core: 'miamicoin-core-v2',
-      token: 'miamicoin-token-v2',
-      deployer: 'SP1H1733V5MZ3SZ9XRW9FKYGEZT0JDGEB8Y634C7R',
+    deployed: true,
+    deployer: 'SP1H1733V5MZ3SZ9XRW9FKYGEZT0JDGEB8Y634C7R',
+    auth: {
+      name: 'miamicoin-auth-v2',
+      initialized: true,
     },
-    status: {
-      activated: false,
-      deployed: false,
+    core: {
+      name: 'miamicoin-core-v2',
+      activated: true,
+      startBlock: 58921,
       shutdown: false,
     },
     token: {
+      name: 'miamicoin-token-v2',
+      activated: true,
+      activationBlock: 24497,
       displayName: 'MiamiCoin',
-      name: 'miamicoin',
+      tokenName: 'miamicoin',
       symbol: 'MIA',
       decimals: 6,
       logo: 'https://cdn.citycoins.co/logos/miamicoin.png',
       uri: 'https://cdn.citycoins.co/metadata/miamicoin.json',
-    }
+    },
   }
 }
 
 // NEWYORKCITYCOIN
 
-const nycConfig: CityConfig = {
+const nycConfig: CityVersions = {
   'v1': {
     cityName: 'New York City',
-    contracts: {
-      auth: 'newyorkcitycoin-auth',
-      core: 'newyorkcitycoin-core-v1',
-      token: 'newyorkcitycoin-token',
-      deployer: 'SP2H8PY27SEZ03MWRKS5XABZYQN17ETGQS3527SA5',
+    deployed: true,
+    deployer: 'SP2H8PY27SEZ03MWRKS5XABZYQN17ETGQS3527SA5',
+    auth: {
+      name: 'newyorkcitycoin-auth',
+      initialized: true,
     },
-    status: {
+    core: {
+      name: 'newyorkcitycoin-core-v1',
       activated: false,
-      deployed: false,
-      shutdown: false,
+      startBlock: 37449,
+      shutdown: true,
+      shutdownBlock: 58922,
     },
     token: {
+      name: 'newyorkcitycoin-token',
+      activated: true,
+      activationBlock: 37449,
       displayName: 'NewYorkCityCoin',
-      name: 'newyorkcitycoin',
+      tokenName: 'newyorkcitycoin',
       symbol: 'NYC',
       decimals: 0,
-      logo: '',
-      uri: '',
-    }
+      logo: 'https://cdn.citycoins.co/logos/newyorkcitycoin.png',
+      uri: 'https://cdn.citycoins.co/metadata/newyorkcitycoin.json',
+    },
   },
   'v2': {
     cityName: 'New York City',
-    contracts: {
-      auth: 'newyorkcitycoin-auth-v2',
-      core: 'newyorkcitycoin-core-v2',
-      token: 'newyorkcitycoin-token-v2',
-      deployer: 'SPSCWDV3RKV5ZRN1FQD84YE1NQFEDJ9R1F4DYQ11',
+    deployed: true,
+    deployer: 'SPSCWDV3RKV5ZRN1FQD84YE1NQFEDJ9R1F4DYQ11',
+    auth: {
+      name: 'newyorkcitycoin-auth-v2',
+      initialized: true,
     },
-    status: {
-      activated: false,
-      deployed: false,
+    core: {
+      name: 'newyorkcitycoin-core-v2',
+      activated: true,
+      startBlock: 58925,
       shutdown: false,
     },
     token: {
+      name: 'newyorkcitycoin-token-v2',
+      activated: true,
+      activationBlock: 37449,
       displayName: 'NewYorkCityCoin',
-      name: 'newyorkcitycoin',
+      tokenName: 'newyorkcitycoin',
       symbol: 'NYC',
       decimals: 6,
-      logo: '',
-      uri: '',
-    }
+      logo: 'https://cdn.citycoins.co/logos/newyorkcitycoin.png',
+      uri: 'https://cdn.citycoins.co/metadata/newyorkcitycoin.json',
+    },
   }
 }
+
 
 export async function getCityConfig(city: string, version: string): Promise<CityVersion> {
   version = version.toLowerCase()
