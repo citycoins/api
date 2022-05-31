7861dda0afa66ca73665fede5ca42938d66bb2af
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-05-31 10:36:42 -0700

    fix: modify activation handlers to use new response format

--- src/handlers/activation/getactivationtarget.ts
+++ src/handlers/activation/getactivationtarget.ts
@@ -1,27 +1,37 @@
 import { Request as IttyRequest } from 'itty-router'
 import { getActivationTarget } from '../../lib/citycoins'
-import { createSingleValue } from '../../lib/common'
+import { createResponse } from '../../lib/common'
 import { CityConfig, getCityConfig } from '../../types/cities'
+import { SingleValue } from '../../types/common'
 
 const GetActivationTarget = async (request: IttyRequest): Promise<Response> => {
   let cityConfig: CityConfig
   let activationTarget: string
+  let response: SingleValue | boolean | number | string
   // check inputs
   const version = request.params?.version ?? undefined
   const city = request.params?.cityname ?? undefined
   if (version === undefined || city === undefined) {
-    return new Response(`Invalid request, missing parameter(s)`, { status: 400 })
+    return new Response(`Invalid request, missing parameter(s)`, {
+      status: 400,
+    })
+  }
+  // check response output format
+  let format = 'json'
+  const { query } = request
+  if (Object.prototype.hasOwnProperty.call(query, 'format')) {
+    if (query?.format !== undefined) format = query.format
   }
   // get/calculate response
   try {
     cityConfig = await getCityConfig(city, version)
     activationTarget = await getActivationTarget(cityConfig)
+    response = await createResponse(activationTarget, format)
   } catch (err) {
     if (err instanceof Error) return new Response(err.message, { status: 404 })
     return new Response(String(err), { status: 404 })
   }
   // return response
-  const response = await createSingleValue(activationTarget)
   return new Response(JSON.stringify(response))
 }
 
--- src/handlers/activation/getuserid.ts
+++ src/handlers/activation/getuserid.ts
@@ -1,17 +1,27 @@
 import { Request as IttyRequest } from 'itty-router'
 import { getUserId } from '../../lib/citycoins'
-import { createSingleValue } from '../../lib/common'
+import { createResponse } from '../../lib/common'
 import { CityConfig, getCityConfig } from '../../types/cities'
+import { SingleValue } from '../../types/common'
 
 const GetUserId = async (request: IttyRequest): Promise<Response> => {
   let cityConfig: CityConfig
   let userId: string
+  let response: SingleValue | boolean | number | string
   // check inputs
   const version = request.params?.version ?? undefined
   const city = request.params?.cityname ?? undefined
   const user = request.params?.address ?? undefined
   if (version === undefined || city === undefined || user === undefined) {
-    return new Response(`Invalid request, missing parameter(s)`, { status: 400 })
+    return new Response(`Invalid request, missing parameter(s)`, {
+      status: 400,
+    })
+  }
+  // check response output format
+  let format = 'json'
+  const { query } = request
+  if (Object.prototype.hasOwnProperty.call(query, 'format')) {
+    if (query?.format !== undefined) format = query.format
   }
   // get/calculate response
   try {
@@ -20,12 +30,12 @@ const GetUserId = async (request: IttyRequest): Promise<Response> => {
     if (userId === null) {
       return new Response(`Address not found: ${user}`, { status: 404 })
     }
+    response = await createResponse(userId, format)
   } catch (err) {
     if (err instanceof Error) return new Response(err.message, { status: 404 })
     return new Response(String(err), { status: 404 })
   }
   // return response
-  const response = await createSingleValue(userId)
   return new Response(JSON.stringify(response))
 }
 
