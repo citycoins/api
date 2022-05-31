ee259ab0b8be4281f7c497a29217fc34cfcbe4ba
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-05-31 11:04:38 -0700

    fix: modify stacks handlers to use new response format

--- src/handlers/stacks/getbnsname.ts
+++ src/handlers/stacks/getbnsname.ts
@@ -1,26 +1,38 @@
 import { Request as IttyRequest } from 'itty-router'
-import { createSingleValue } from '../../lib/common'
+import { createResponse } from '../../lib/common'
 import { getBnsName } from '../../lib/stacks'
+import { SingleValue } from '../../types/common'
 
 const GetBnsName = async (request: IttyRequest): Promise<Response> => {
   let bnsNames: string
+  let response: SingleValue | boolean | number | string
   // check inputs
   const address = request.params?.address ?? undefined
   if (address === undefined) {
-    return new Response(`Invalid request, missing parameter(s)`, { status: 400 })
+    return new Response(`Invalid request, missing parameter(s)`, {
+      status: 400,
+    })
+  }
+  // check response output format
+  let format = 'json'
+  const { query } = request
+  if (Object.prototype.hasOwnProperty.call(query, 'format')) {
+    if (query?.format !== undefined) format = query.format
   }
   // get/calculate response
   try {
     bnsNames = await getBnsName(address)
     if (bnsNames === 'undefined') {
-      return new Response(`BNS name(s) not found for address: ${address}`, { status: 404 })
+      return new Response(`BNS name(s) not found for address: ${address}`, {
+        status: 404,
+      })
     }
+    response = await createResponse(bnsNames, format)
   } catch (err) {
     if (err instanceof Error) return new Response(err.message, { status: 404 })
     return new Response(String(err), { status: 404 })
   }
   // return response
-  const response = await createSingleValue(bnsNames)
   return new Response(JSON.stringify(response))
 }
 
--- src/handlers/stacks/getdateatblock.ts
+++ src/handlers/stacks/getdateatblock.ts
@@ -1,13 +1,23 @@
 import { Request as IttyRequest } from 'itty-router'
-import { createSingleValue, isStringAllDigits } from '../../lib/common'
+import { createResponse, isStringAllDigits } from '../../lib/common'
 import { getDateAtBlock, getStacksBlockHeight } from '../../lib/stacks'
+import { SingleValue } from '../../types/common'
 
 const GetDateAtBlock = async (request: IttyRequest): Promise<Response> => {
   let dateAtBlock: string
+  let response: SingleValue | boolean | number | string
   // check inputs
   let blockHeight = request.params?.blockHeight ?? undefined
   if (blockHeight === undefined) {
-    return new Response(`Invalid request, missing parameter(s)`, { status: 400 })
+    return new Response(`Invalid request, missing parameter(s)`, {
+      status: 400,
+    })
+  }
+  // check response output format
+  let format = 'json'
+  const { query } = request
+  if (Object.prototype.hasOwnProperty.call(query, 'format')) {
+    if (query?.format !== undefined) format = query.format
   }
   // get/calculate response
   try {
@@ -15,16 +25,18 @@ const GetDateAtBlock = async (request: IttyRequest): Promise<Response> => {
       blockHeight = await getStacksBlockHeight()
     } else {
       if (!isStringAllDigits(blockHeight)) {
-        return new Response(`Block height not specified or invalid`, { status: 400 })
+        return new Response(`Block height not specified or invalid`, {
+          status: 400,
+        })
       }
     }
     dateAtBlock = await getDateAtBlock(blockHeight)
+    response = await createResponse(dateAtBlock, format)
   } catch (err) {
     if (err instanceof Error) return new Response(err.message, { status: 404 })
     return new Response(String(err), { status: 404 })
   }
   // return response
-  const response = await createSingleValue(dateAtBlock)
   return new Response(JSON.stringify(response))
 }
 
